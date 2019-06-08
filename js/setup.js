2193181d2de179a7a1ee0c7335a416ac0fd4f59f
Gleb Dolgushin <dolgushing@gmail.com>
2019-06-08 16:47:29 +0300

    правки

---
+++ js/setup.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// Показываем блок .setup, убрав в JS-коде у него класс .hidden.
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+// Создаём массивы
+var WIZARDS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARDS_LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARDS_COATS_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARDS_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+// Получаем случайные параметры волшебника
+var generateRandomName = function () {
+  var WizardName = WIZARDS_NAMES[Math.floor(Math.random() * WIZARDS_NAMES.length)] + ' ' + WIZARDS_LASTNAMES[Math.floor(Math.random() * WIZARDS_LASTNAMES.length)];
+  return WizardName;
+};
+
+var generateRandomWizardColor = function () {
+  var WizardCoatColor = WIZARDS_COATS_COLORS[Math.floor(Math.random() * WIZARDS_COATS_COLORS.length)];
+  return WizardCoatColor;
+};
+
+var generateRandomEyesColor = function () {
+  var WizardEyesColor = WIZARDS_EYES_COLORS[Math.floor(Math.random() * WIZARDS_EYES_COLORS.length)];
+  return WizardEyesColor;
+};
+
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+// Создаём массив, состоящий из 4 сгенерированных JS объектов
+var generateWizards = function () {
+  var wizards = [];
+  for (var i = 0; i < 4; i++) {
+    wizards[i] = {name: generateRandomName(), coatColor: generateRandomWizardColor(), eyesColor: generateRandomEyesColor()};
+  }
+  return wizards;
+};
+
+// Клонируем шаблон и заполняем данными волшебника
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+
+// Добавляем клонированный шаблон на страницу и заполняем его случайными данными
+var wizards = generateWizards(); // Заполнили массив данных волшебников
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+
+// Покажем блок .setup-similar, удалив у него CSS-класс hidden.
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
