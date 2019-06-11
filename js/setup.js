7bb31bc16b0fb3b5bbd052a27f0acbb20a674f86
Gleb Dolgushin <dolgushing@gmail.com>
2019-06-11 15:40:08 +0300

    правки6

--- js/setup.js
+++ js/setup.js
@@ -1,21 +1,37 @@
 'use strict';
-
-// Показываем блок .setup, убрав в JS-коде у него класс .hidden.
-var showElement = function (hiddenElementClass) {
-  document.querySelector(hiddenElementClass).classList.remove('hidden');
-};
-
-showElement('.setup');
-
 // Создаём массивы
 var WIZARDS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARDS_LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARDS_COATS_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARDS_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
+// Показываем блок .setup, убрав в JS-коде у него класс .hidden.
+var showSetup = function () {
+  document.querySelector('.setup').classList.remove('hidden');
+};
+showSetup();
+
 // Генерируем случайно число
 var generateRandomNumber = function (max) {
   return Math.floor(Math.random() * max);
+};
+
+// Генерируем случайное свойство волшебника
+var generateWizardProperty = function (arrayName) {
+  return arrayName[generateRandomNumber(arrayName.length)];
+};
+
+// Создаём массив, состоящий из сгенерированных JS объектов
+var generateWizardsArray = function (numberOfWizards) {
+  var wizards = [];
+  for (var i = 0; i < numberOfWizards; i++) {
+    wizards[i] = {
+      name: generateWizardProperty(WIZARDS_NAMES) + ' ' + generateWizardProperty(WIZARDS_LASTNAMES),
+      coatColor: generateWizardProperty(WIZARDS_COATS_COLORS),
+      eyesColor: generateWizardProperty(WIZARDS_EYES_COLORS)
+    };
+  }
+  return wizards;
 };
 
 // Находим элемент в который будем вставлять новые элементы
@@ -26,39 +42,30 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
-// Создаём массив, состоящий из 4 сгенерированных JS объектов
-var generateWizards = function () {
-  var numberOfWizards = 4;
-  var wizards = [];
-  for (var i = 0; i < numberOfWizards; i++) {
-    wizards[i] = {
-      name: WIZARDS_NAMES[generateRandomNumber(WIZARDS_NAMES.length)] + ' ' + WIZARDS_LASTNAMES[generateRandomNumber(WIZARDS_LASTNAMES.length)],
-      coatColor: WIZARDS_COATS_COLORS[generateRandomNumber(WIZARDS_COATS_COLORS.length)],
-      eyesColor: WIZARDS_EYES_COLORS[generateRandomNumber(WIZARDS_EYES_COLORS.length)]};
-  }
-  return wizards;
-};
-
 // Клонируем шаблон и заполняем данными волшебника
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-
   return wizardElement;
 };
 
-// Заполнили массив данных волшебников
-var wizards = generateWizards();
-// Складываем новые элементы в контейцнер
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
+// Заполнили массив данных волшебников Складываем новые элементы в контейцнер
+var addToFragment = function (wizards) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  return fragment;
+};
+
 // Добавляем элементы из контейцнера на страницу
-similarListElement.appendChild(fragment);
+var numberOfWizards = 4;
+similarListElement.appendChild(addToFragment(generateWizardsArray(numberOfWizards)));
 
 // Покажем блок .setup-similar, удалив у него CSS-класс hidden.
-showElement('.setup-similar');
+var showSetupSimilar = function () {
+  document.querySelector('.setup-similar').classList.remove('hidden');
+};
+showSetupSimilar();
